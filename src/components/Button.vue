2cefe0016ca865d8d9abe0e02c47dc99d996d1c7
locolin <locolin99@gmail.com>
2023-07-26 01:51:59 +0800

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+	<button @click="onClick()" :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "Button",
+	props: {
+		text: String,
+		color: String,
+	},
+    methods: {
+        onClick(){
+            console.log("click")
+        }
+    }
+});
+</script>
